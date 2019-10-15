a552000b98b2b72d0703592136195da24ab06f1d
chanchal kumar <chanchalshahkumar@gmail.com>
2019-10-15 23:17:24 +0530

    add security

--- src/actions/securityActions.js
+++ src/actions/securityActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setJWTToken from "../securityUtils/setJWTTokens";
+import jwt_decode from "jwt-decode";
 
 export const createNewUser = (newUser, history) => async dispatch => {
   try {
@@ -8,6 +10,31 @@ export const createNewUser = (newUser, history) => async dispatch => {
     dispatch({
       type: GET_ERRORS,
       payload: {}
+    });
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    });
+  }
+};
+
+export const login = LoginRequest => async dispatch => {
+  try {
+    // post => Login Request
+    const res = await axios.post("/api/users/login", LoginRequest);
+    // Extract token from res.data
+    const { token } = res.data;
+    //store token in localStorage
+    localStorage.setItem("jwtToken", token);
+    // set ouer token in header
+    setJWTToken(token);
+    // decode the token on React
+    const decoded = jwt_decode(token);
+    // dispatch to our securityReducer
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: decoded
     });
   } catch (err) {
     dispatch({
